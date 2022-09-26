30465632d183fbc97fd054307c8fe0c6d0263ad0
ReyesBF <49005780+ReyesBF@users.noreply.github.com>
2022-09-26 09:20:03 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main{
-  public static void main (Strig[] arg){
+  public static void main (String[] arg){
     coche miCoche = new coche();
-    miCoche.sumarPuertas();
+    miCoche.sumarPuerta();
     System.out.println("Hola");
     System.out.println("Las puertas de tu coche son: "+miCoche.puertas);
   }
   public static class coche{
-    int puertas = 0;
-    public static class sumarPuerta(){
-      puertas ++;
+      int puertas = 0;
+      public void sumarPuerta(){
+          puertas ++;
     }
   }
 }
